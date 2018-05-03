023bcb39b605e9ca48edee34efc87d16240006e1
Chiara Machello <chiara.machello@monash.edu>
2018-05-03 14:00:25 +1000

    Correction of previous version

--- test4.f90
+++ test4.f90
@@ -7,17 +7,18 @@ contains
 recursive function factorial(n) result(factorial_result)
 	
 	integer, intent(in) :: n
-	integer :: factorial result
+	integer :: factorial_result
 
 	if(n < 0) then
  	 print *,"Input Error"
-	 else if (n +== 0) then
+     stop
+	 else if (n == 0) then
   	 factorial_result = 1
 	else 
-  	 factorial result = n*factorial(n-1)
+  	 factorial_result = n*factorial(n-1)
 	end if
 
-end funcion factorial
+end function factorial
 
 end module factorial_module
 
@@ -25,7 +26,8 @@ program test_factorial
 
 	use factorial_module
 	implicit none
-	integer :: num
+	integer :: num, factorial_result
+    print *, "Insert a number"
 	read *, num
 	factorial_result = factorial(num)
 	print *, "The result of factorial ", num, "is", factorial_result
